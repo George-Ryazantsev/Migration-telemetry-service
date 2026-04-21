0026eaa00877b7954d33182a3fa7e6ab65b8283a
georgiy_ryazantsev <ryazantsevgeo@gmail.com>
2026-04-21 23:42:55 +0300

    Добавьте файлы проекта.

---
+++ Migration telemetry service/Controllers/MetricsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Migration_telemetry_service.Controllers
+{
+    [ApiController]
+    [Route("api/metrics")]
+    public class MetricsController : ControllerBase
+    {
+
+    }
+}
